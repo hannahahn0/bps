ecafa7204b466046f19e76d9b0f7b15cf52ef53a
Philip Papurt <ginkoid@gmail.com>
2018-03-07 22:22:05 -0500

    Add security headers to lambda functions too

--- src/lambda/with/withApiCb.js
+++ src/lambda/with/withApiCb.js
@@ -6,6 +6,12 @@ export default wrapped => (evt, ctx, lambdaCb) => wrapped(evt, ctx, (result, dat
         statusCode: response.status,
         headers: {
             'Content-Type': 'application/json',
+            'Referrer-Policy': 'origin',
+            'X-Content-Type-Options': 'nosniff',
+            'X-Dns-Prefetch-Control': 'off',
+            'X-Download-Options': 'noopen',
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Xss-Protection': '1; mode=block',
         },
         body: JSON.stringify({
             code: response.code,
